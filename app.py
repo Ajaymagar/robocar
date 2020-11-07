d752766b861f928cc5403ca7a00e6557418512d7
Ajaymagar <ajaymagar4818@gmail.com>
2020-11-07 13:02:36 +0530

    3rd

--- app.py
+++ app.py
@@ -6,16 +6,28 @@ app = Flask(__name__)
 video = cv2.VideoCapture(0)
 
 face_cascade  = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-
+#maskNet = load_model('')
 @app.route('/')
 def index():
     return render_template('index.html')
+'''
+def detectAndPredict():
+    locs = []
+    faces = []
+    preds = []
+    faces_in_image = face_cascade.detectMultiScale(frame , 1.3 ,5)
+    for (x , y , w,h) in faces_in_image:
+        locs.append(())
+    preds = maskNet.predict(faces)
 
+    return (locs , pred)
+'''
 def gen(video):
     while True:
         success, image = video.read()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces_in_image = face_cascade.detectMultiScale(gray , 1.3 ,5)
+        print(faces_in_image)
         for (x,y,w,h) in faces_in_image:
             cv2.rectangle(image , (x,y),(x+w,y+h),(0,255,0),2)
             break
